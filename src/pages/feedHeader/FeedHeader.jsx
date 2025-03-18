67d1354e0dddb2dd4a97f5725dcc411e5778cf9d
Dinesh Pawar <dsp70458@gmail.com>
2025-03-18 08:16:45 +0530

    Fix missing CSS file

--- src/pages/feedHeader/FeedHeader.jsx
+++ src/pages/feedHeader/FeedHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./feedHeader.css";
+import "./FeedHeader.css";
 
 const Feedheader = () => {
   const [activeTab, setActiveTab] = useState("posts");
